c41e2c729980d8f3de29a2fea96a032b0ffa077b
kubanm3 <kubanm3@gmail.com>
2019-11-25 20:38:45 +0100

    added insert,edit, delete and view to db
    added buttons in menu for actions regarding db

--- ECard/app/src/main/java/com/ECard/AddLayout.java
+++ ECard/app/src/main/java/com/ECard/AddLayout.java
@@ -1,27 +1,150 @@
 package com.ECard;
 
+import android.app.AlertDialog;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
 
 public class AddLayout extends ActionBarActivity {
-    private Button mBtGoBack;
+
+    DatabaseHelper myDb;
+    EditText editName, editOrientation, editNameX, editNameY, editCompanyX, editCompanyY,
+            editAddressX,
+            editAddressY, editEmailX, editEmailY, editPhoneX, editPhoneY, editId;
+    Button btnAddData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_layout);
+        myDb = new DatabaseHelper(this);
 
-        mBtGoBack = (Button) findViewById(R.id.bt_go_back);
-        mBtGoBack.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                finish();
-            }
-        });
+        editName = (EditText) findViewById(R.id.layoutName);
+        editOrientation = (EditText) findViewById(R.id.layoutOrient);
+        editNameX = (EditText) findViewById(R.id.layoutNameX);
+        editNameY = (EditText) findViewById(R.id.layoutNameY);
+        editCompanyX = (EditText) findViewById(R.id.layoutCompanyX);
+        editCompanyY = (EditText) findViewById(R.id.layoutCompanyY);
+        editAddressX = (EditText) findViewById(R.id.layoutAddressX);
+        editAddressY = (EditText) findViewById(R.id.layoutAddressY);
+        editEmailX = (EditText) findViewById(R.id.layoutEmailX);
+        editEmailY = (EditText) findViewById(R.id.layoutEmailY);
+        editPhoneX = (EditText) findViewById(R.id.layoutPhoneX);
+        editPhoneY = (EditText) findViewById(R.id.layoutPhoneY);
+        editId = (EditText) findViewById(R.id.layoutID);
+        btnAddData = (Button) findViewById(R.id.saveLayoutBT);
+
+        AddData();
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_add_layout, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.about:
+                Toast.makeText(this, "Autor: Jakub Legutko", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.editFromList:
+                UpdateData();
+                return true;
+            case R.id.deleteFromList:
+                DeleteData();
+                return true;
+            case R.id.showFromList:
+                viewAll();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void DeleteData() {
+        Integer deletedRows = myDb.deleteData(editId.getText().toString());
+        if (deletedRows > 0)
+            Toast.makeText(AddLayout.this, "Data deleted", Toast.LENGTH_LONG).show();
+        else
+            Toast.makeText(AddLayout.this, "Data not deleted", Toast.LENGTH_LONG).show();
+    }
 
 
+    public void UpdateData() {
+        boolean isUpdate =
+                myDb.updateData(editId.getText().toString(), editName.getText().toString(),
+                        editOrientation.getText().toString(),
+                        editNameX.getText().toString(), editNameY.getText().toString(),
+                        editCompanyX.getText().toString(), editCompanyY.getText().toString(),
+                        editAddressX.getText().toString(), editAddressY.getText().toString(),
+                        editEmailX.getText().toString(), editEmailY.getText().toString(),
+                        editPhoneX.getText().toString(), editPhoneY.getText().toString());
+        if (isUpdate)
+            Toast.makeText(AddLayout.this, "Data updated", Toast.LENGTH_LONG).show();
+        else
+            Toast.makeText(AddLayout.this, "Data not updated", Toast.LENGTH_LONG).show();
+    }
+
+    public void AddData() {
+        btnAddData.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        boolean isInserted = myDb.insertData(editName.getText().toString(),
+                                editOrientation.getText().toString(),
+                                editNameX.getText().toString(), editNameY.getText().toString(),
+                                editCompanyX.getText().toString(), editCompanyY.getText().toString(),
+                                editAddressX.getText().toString(), editAddressY.getText().toString(),
+                                editEmailX.getText().toString(), editEmailY.getText().toString(),
+                                editPhoneX.getText().toString(), editPhoneY.getText().toString());
+                        if (isInserted)
+                            Toast.makeText(AddLayout.this, "Data inserted", Toast.LENGTH_LONG).show();
+                        else
+                            Toast.makeText(AddLayout.this, "Data not inserted", Toast.LENGTH_LONG).show();
+                    }
+                }
+        );
+    }
+
+    public void viewAll() {
+        Cursor res = myDb.getAllData();
+        if (res.getCount() == 0) {
+            // show message
+            showMessage("Error", "Nothing found");
+            return;
+        }
+
+        StringBuffer buffer = new StringBuffer();
+        while (res.moveToNext()) {
+            buffer.append("Id :" + res.getString(0) + "\n");
+            buffer.append("Name :" + res.getString(1) + "\n");
+            buffer.append("Surname :" + res.getString(2) + "\n");
+            buffer.append("Marks :" + res.getString(3) + "\n\n");
+        }
+
+        // Show all data
+        showMessage("Data", buffer.toString());
+    }
+
+    public void showMessage(String title, String Message) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle(title);
+        builder.setMessage(Message);
+        builder.show();
     }
 
 }
--- ECard/app/src/main/java/com/ECard/DatabaseHelper.java
+++ ECard/app/src/main/java/com/ECard/DatabaseHelper.java
@@ -29,33 +29,33 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("create table " + TABLE_NAME +" (ID INTEGER PRIMARY KEY AUTOINCREMENT,LAYOUT_NAME TEXT,ORIENTATION INTEGER,NAME_POS_Y INTEGER,COMPANY_POS_X INTEGER," +
+        db.execSQL("create table " + TABLE_NAME + " (ID INTEGER PRIMARY KEY AUTOINCREMENT,LAYOUT_NAME TEXT,ORIENTATION INTEGER,NAME_POS_X INTEGER,NAME_POS_Y INTEGER,COMPANY_POS_X INTEGER," +
                 "COMPANY_POS_Y INTEGER,ADDRESS_POS_X INTEGER,ADDRESS_POS_Y INTEGER,EMAIL_POS_X INTEGER,EMAIL_POS_Y INTEGER,PHONE_POS_X INTEGER,PHONE_POS_Y INTEGER)");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE IF EXISTS "+TABLE_NAME);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
         onCreate(db);
     }
 
-    public boolean insertData(String layout_name,String orientatnion,String name_pos_x,String name_pos_y,String company_pos_x,String company_pos_y,String address_pos_x,String address_pos_y,String email_pos_x,String email_pos_y,String phone_pos_x,String phone_pos_y) {
+    public boolean insertData(String layout_name, String orientation, String name_pos_x, String name_pos_y, String company_pos_x, String company_pos_y, String address_pos_x, String address_pos_y, String email_pos_x, String email_pos_y, String phone_pos_x, String phone_pos_y) {
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
-        contentValues.put(COL_2,layout_name);
-        contentValues.put(COL_3,orientatnion);
-        contentValues.put(COL_4,name_pos_x);
-        contentValues.put(COL_5,name_pos_y);
-        contentValues.put(COL_6,company_pos_x);
-        contentValues.put(COL_7,company_pos_y);
-        contentValues.put(COL_8,address_pos_x);
-        contentValues.put(COL_9,address_pos_y);
-        contentValues.put(COL_10,email_pos_x);
-        contentValues.put(COL_11,email_pos_y);
-        contentValues.put(COL_12,phone_pos_x);
-        contentValues.put(COL_13,phone_pos_y);
-        long result = db.insert(TABLE_NAME,null ,contentValues);
-        if(result == -1)
+        contentValues.put(COL_2, layout_name);
+        contentValues.put(COL_3, orientation);
+        contentValues.put(COL_4, name_pos_x);
+        contentValues.put(COL_5, name_pos_y);
+        contentValues.put(COL_6, company_pos_x);
+        contentValues.put(COL_7, company_pos_y);
+        contentValues.put(COL_8, address_pos_x);
+        contentValues.put(COL_9, address_pos_y);
+        contentValues.put(COL_10, email_pos_x);
+        contentValues.put(COL_11, email_pos_y);
+        contentValues.put(COL_12, phone_pos_x);
+        contentValues.put(COL_13, phone_pos_y);
+        long result = db.insert(TABLE_NAME, null, contentValues);
+        if (result == -1)
             return false;
         else
             return true;
@@ -63,32 +63,32 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     public Cursor getAllData() {
         SQLiteDatabase db = this.getWritableDatabase();
-        Cursor res = db.rawQuery("select layout_name from "+TABLE_NAME,null);
+        Cursor res = db.rawQuery("select * from " + TABLE_NAME, null);
         return res;
     }
 
-    public boolean updateData(String id,String layout_name,String orientatnion,String name_pos_x,String name_pos_y,String company_pos_x,String company_pos_y,String address_pos_x,String address_pos_y,String email_pos_x,String email_pos_y,String phone_pos_x,String phone_pos_y) {
+    public boolean updateData(String id, String layout_name, String orientation, String name_pos_x, String name_pos_y, String company_pos_x, String company_pos_y, String address_pos_x, String address_pos_y, String email_pos_x, String email_pos_y, String phone_pos_x, String phone_pos_y) {
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
-        contentValues.put(COL_1,id);
-        contentValues.put(COL_2,layout_name);
-        contentValues.put(COL_3,orientatnion);
-        contentValues.put(COL_4,name_pos_x);
-        contentValues.put(COL_5,name_pos_y);
-        contentValues.put(COL_6,company_pos_x);
-        contentValues.put(COL_7,company_pos_y);
-        contentValues.put(COL_8,address_pos_x);
-        contentValues.put(COL_9,address_pos_y);
-        contentValues.put(COL_10,email_pos_x);
-        contentValues.put(COL_11,email_pos_y);
-        contentValues.put(COL_12,phone_pos_x);
-        contentValues.put(COL_13,phone_pos_y);
-        db.update(TABLE_NAME, contentValues, "ID = ?",new String[] { id });
+        contentValues.put(COL_1, id);
+        contentValues.put(COL_2, layout_name);
+        contentValues.put(COL_3, orientation);
+        contentValues.put(COL_4, name_pos_x);
+        contentValues.put(COL_5, name_pos_y);
+        contentValues.put(COL_6, company_pos_x);
+        contentValues.put(COL_7, company_pos_y);
+        contentValues.put(COL_8, address_pos_x);
+        contentValues.put(COL_9, address_pos_y);
+        contentValues.put(COL_10, email_pos_x);
+        contentValues.put(COL_11, email_pos_y);
+        contentValues.put(COL_12, phone_pos_x);
+        contentValues.put(COL_13, phone_pos_y);
+        db.update(TABLE_NAME, contentValues, "ID = ?", new String[]{id});
         return true;
     }
 
-    public Integer deleteData (String id) {
+    public Integer deleteData(String id) {
         SQLiteDatabase db = this.getWritableDatabase();
-        return db.delete(TABLE_NAME, "ID = ?",new String[] {id});
+        return db.delete(TABLE_NAME, "ID = ?", new String[]{id});
     }
 }
